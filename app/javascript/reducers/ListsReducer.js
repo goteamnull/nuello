0987f91ee8438fed37a3b2f0b04acb408000cc26
Jay Shenk <jay.shenk@gmail.com>
2017-10-22 16:45:39 -0400

    remove nested cards from listsReducer state

--- app/javascript/reducers/ListsReducer.js
+++ app/javascript/reducers/ListsReducer.js
@@ -1,6 +1,9 @@
 export default function listsReducer(state = [], action) {
   if (action.type === 'FETCH_BOARD_SUCCESS') {
-    const updatedLists = action.board.lists;
+    const updatedLists = action.board.lists.map(list => {
+      const { cards, ...listWithoutCards } = list;
+      return listWithoutCards;
+    });
     // const excludedLists = state.filter((list) => list.board_id !== action.board.id);
     const unchangedLists = state.filter((stateList) => {
       return !updatedLists.some((list) => stateList.id === list.id);
