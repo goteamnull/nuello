f967dc11b2c31beb6bfc361a7b709162cfcb7a30
Tiffany Han <hantiffany@gmail.com>
2017-10-24 10:27:07 -0700

    write tests for react components

---
+++ app/javascript/components/board/ListTitle.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ListTitle from './ListTitle';
+
+import { shallow } from 'enzyme';
+
+describe("ListTitle", () => {
+  it("displays the list title", () => {
+    const wrapper = shallow(
+      <ListTitle
+        title="My list's title"
+        onClick={() => {}}
+      />
+    );
+
+    expect(
+      wrapper.containsMatchingElement(
+        <p>My list's title</p>
+      )
+    ).toBe(true);
+  });
+});
---
+++ app/javascript/components/board/ListTitleForm.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ListTitleForm from './ListTitleForm';
+
+import { shallow } from 'enzyme';
+
+describe("ListTitleForm", () => {
+  it("displays the `title` prop", () => {
+    const wrapper = shallow(
+      <ListTitleForm
+        title="Hello world!"
+        onTextChange={() => {}}
+        onSubmit={() => {}}
+      />
+    );
+
+    expect(
+      wrapper.html().indexOf('value="Hello world!')
+    );
+  });
+});
