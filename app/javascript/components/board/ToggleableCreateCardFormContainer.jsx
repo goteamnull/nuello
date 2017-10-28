98ad276b7c685d02c9017b849df879589d0f8c67
Walid Wahed <wwahed1@gmail.com>
2017-10-27 21:26:40 -0400

    confirm api compatibility

--- app/javascript/components/board/ToggleableCreateCardFormContainer.jsx
+++ app/javascript/components/board/ToggleableCreateCardFormContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ToggleableCreateCardForm from './ToggleableCreateCardForm'
+import PositionCalculator from '../../lib/PositionCalculator'
 
 import * as actions from '../../actions/CardActions'
 
@@ -34,11 +35,23 @@ class ToggleableCreateCardFormContainer extends React.Component {
     this.props.toggleActiveDropdown(null);
   }
 
+  calculatePosition = () => {
+    const cards = this.context.store.getState().cards.filter((card) => (
+      card.list_id === this.props.list_id
+    ));
+    return PositionCalculator(cards, cards.length);
+  };
+
   handleSaveClick = () => {
+    if (this.state.title === '') {
+      return;
+    }
+
     const newCard = {
       list_id: this.props.listId,
       card: {
-       title: this.state.title    
+       title: this.state.title,
+       position: this.calculatePosition() 
       }
     };
 
--- app/javascript/constants/ApiRoutes.js
+++ app/javascript/constants/ApiRoutes.js
@@ -1,7 +1,7 @@
 export const BOARDS_INDEX_URL = '/api/boards';
 export const BOARD_URL        = '/api/boards/';
 export const CREATE_BOARD_URL = '/api/boards';
-export const CREATE_LIST_URL = '/api/lists';
+export const CREATE_LIST_URL  = '/api/lists';
 export const UPDATE_LIST_URL  = '/api/lists/';
-export const CRAETE_CARD_UL  = '/api/cards/';
+export const CREATE_CARD_URL  = '/api/cards/';
 
