db4aa5f7e49913ca774c5173f348416dad668640
Tiffany Han <hantiffany@gmail.com>
2018-03-04 20:26:08 -0800

    add route and controller action to update a card

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -24,9 +24,52 @@ class Api::CardsController < ApplicationController
     render 'api/shared/error', status: :unprocessable_entity
   end
 
+  def update
+    unless card_update_params_valid?
+      @error = 'at least one allowed attribute must be included'
+      render 'api/shared/error', status: :unprocessable_entity
+      return
+    end
+
+    @card = Card.find(params[:id])
+
+    if @card.update_attributes(card_update_params)
+      render :update, status: :ok
+    else
+      @error = @card.errors.full_messages.join(', ')
+      render 'api/shared/error', status: :unprocessable_entity
+    end
+
+  rescue ActiveRecord::RecordNotFound
+    @error = "Invalid card id provided"
+    render 'api/shared/error', status: :not_found
+  end
+
   private
 
   def card_params
     params.require(:card).permit(:title, :position)
+  end
+
+  def card_update_allowed_attributes
+    [
+      :title,
+      :list_id,
+      :position,
+      :description,
+      :archived,
+      :due_date,
+      :completed
+    ]
+  end
+
+  def card_update_params
+    params.require(:card).permit(*card_update_allowed_attributes)
+  end
+
+  def card_update_params_valid?
+    card_update_params.as_json.any? do |k, _|
+      card_update_allowed_attributes.include?(k.to_sym)
+    end
   end
 end
